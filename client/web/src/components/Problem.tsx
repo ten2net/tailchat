4050f434f47c6b95626455b7bad8b6e2627c6bd3
Hickays <Hickays@163.com>
2023-09-05 18:50:56 +0800

    fix: 修改 "收件箱" 和空数据提示的图标

--- client/web/src/components/Problem.tsx
+++ client/web/src/components/Problem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import problemSvg from '@assets/images/problem.svg';
+import emptyDataPng from '@assets/images/empty-data.png';
 import { t } from 'tailchat-shared';
 import clsx from 'clsx';
 
@@ -18,7 +18,7 @@ export const Problem: React.FC<ProblemProps> = React.memo((props) => {
       className={clsx('text-center w-full', props.className)}
       style={props.style}
     >
-      <img className="w-32 h-32 m-auto mb-2" src={problemSvg} />
+      <img className="w-32 h-32 m-auto mb-2" src={emptyDataPng} />
 
       <div>{props.text ?? t('出现了一些问题')}</div>
     </div>
