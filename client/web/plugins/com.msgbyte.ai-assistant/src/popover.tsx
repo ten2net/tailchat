ab30e22c951cc896d0ec430e32c048b2795d5c1f
wangf <ten2net@163.com>
2023-09-05 15:00:21 +0800

    fix: ai助手插件的地址硬编码到192.168.128.68，以便适应环境

--- client/web/plugins/com.msgbyte.ai-assistant/src/popover.tsx
+++ client/web/plugins/com.msgbyte.ai-assistant/src/popover.tsx
@@ -64,7 +64,7 @@ export const AssistantPopover: React.FC<{
       // });
 
       const { data } = await axios.post(
-        'http://192.168.15.130:8001/v1/chat-messages',
+        'http://192.168.128.68:8001/v1/chat-messages',
         {
           inputs: {},
           query: question,
